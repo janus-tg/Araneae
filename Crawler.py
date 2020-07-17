3765b113b60815d80020c179a80f4e9cf83f1156
Tuhin Ghose <ghosetuhin@gmail.com>
2020-07-17 15:34:23 +0530

    wikipedia info crawler added

--- Crawler.py
+++ Crawler.py
@@ -1,14 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input("Enter the URL: ")
+#wikipedia info crawler
 
-result = requests.get(url)
+def wikiCrawler():
 
-if result.status_code == 200:
-    print(result)
+    #getting the url from the user
+    url = input("Enter the Wikipedia URL: ")
+    result = requests.get(url)
 
-else: 
-    print("Incorrect URL!")
+    #if else codeblock if the code is succesful
 
+    if result.status_code == 200:
+        src = result.content
+        soup = BeautifulSoup(src, "lxml")
+        links = soup.find_all(["h2", "h3", "p"])
+        for link in links:
+             print(link.text)
+    else: 
+        print("Incorrect URL! Enter a correct URL.")
 
+   
