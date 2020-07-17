446a8e7a5b4ad41e696c03a3165fedd4c337dc47
Tuhin Ghose <ghosetuhin@gmail.com>
2020-07-17 16:27:40 +0530

    added support for text file creation

--- Crawler.py
+++ Crawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 #wikipedia info crawler
 
@@ -12,12 +13,40 @@ def wikiCrawler():
     #if else codeblock if the code is succesful
 
     if result.status_code == 200:
+
         src = result.content
         soup = BeautifulSoup(src, "lxml")
+
+        # finding all the h2, h3 and p tags
         links = soup.find_all(["h2", "h3", "p"])
-        for link in links:
-             print(link.text)
+        pageName = soup.find("h1")
+
+        #creating a .txt file to save the info
+        with open (pageName.text + '.txt', 'w', encoding = "utf-8") as fp: 
+
+            #going through each tag and writing the info on the .txt file
+            for link in links:
+                fp.write(link.text)
+
+        while True:
+                us = input("Would you like to open the current text file? (Enter Yes or No): ")
+
+                if us == "Yes":
+                   with open(pageName.text + '.txt', 'r') as f:
+                        print("\n\n")
+                        print(f.read())
+                        break
+
+                elif us == "No":
+                   print("File was created succesfully. Exiting program...")
+                   break
+
+                else: 
+                    print("Incorrect input. Try again.")
+
     else: 
+
         print("Incorrect URL! Enter a correct URL.")
 
-   
+print("Hi! Welcome to Aranae!")
+wikiCrawler()
